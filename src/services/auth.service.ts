db16aecfc886155a84a2ed8c2e3fe64c3a38dc6a
berdo <vaberdini@gmail.com>
2026-08-27 10:39:39 -0300

    feat: login con JWT

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,12 @@
 import bcrypt from "bcryptjs";
-import { findUserByEmail, createUser } from "../repositories/user.repository";
+import jwt from "jsonwebtoken";
+import {
+  findUserByEmail,
+  createUser,
+  findUserByEmailWithPassword,
+} from "../repositories/user.repository";
 import { createInitialBalances } from "../repositories/balance.repository";
-import { createWallet } from '../repositories/wallet.repository';
+import { createWallet } from "../repositories/wallet.repository";
 
 export async function registerUser(
   email: string,
@@ -23,4 +28,28 @@ export async function registerUser(
   await createInitialBalances(wallet.id);
 
   return newUser;
+}
+
+export async function loginUser(email: string, password: string) {
+  const user = await findUserByEmailWithPassword(email);
+
+  if (!user) {
+    throw new Error("Credenciales inválidas");
+  }
+
+  const passwordMatches = await bcrypt.compare(password, user.password_hash);
+
+  if (!passwordMatches) {
+    throw new Error("Credenciales inválidas");
+  }
+
+  const token = jwt.sign(
+    { userId: user.id },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "1d" },
+  );
+
+  const { password_hash, ...userWithoutPassword } = user;
+
+  return { token, user: userWithoutPassword };
 }
