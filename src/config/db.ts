dac5e8304b422d6b0680e8b76e491fab4a73c3cc
berdo <vaberdini@gmail.com>
2026-08-31 22:54:40 -0300

    fix: mover dotenv.config() a server.ts y agregar GOOGLE_CLIENT_ID a .env.example

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,6 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import { Pool } from 'pg';
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
