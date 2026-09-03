b9ca8f77145ab68222eec12251fa753607b7d93b
berdo <vaberdini@gmail.com>
2026-09-03 18:26:36 -0300

    feat: transferencias entre usuarios, con umbral de monto alto y historial combinado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import userRoutes from "./user.routes";
 import transactionRoutes from "./transaction.routes";
 import chatbotRoutes from "./chatbot.routes";
 import ratesRoutes from "./rates.routes";
+import transferRoutes from './transfer.routes';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use("/chatbot", chatbotRoutes);
 router.use("/users", userRoutes);
 router.use("/transactions", transactionRoutes);
 router.use("/rates", ratesRoutes);
+router.use('/transfers', transferRoutes);
 
 export default router;
