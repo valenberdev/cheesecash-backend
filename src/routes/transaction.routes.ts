90f61e13051bba6a49e991982a1678e439d114a7
berdo <vaberdini@gmail.com>
2026-09-04 13:08:19 -0300

    feat: validacion con zod en creacion de transacciones

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createTransactionEndpoint, getHistory, confirmTransactionEndpoint } from "../controllers/transaction.controller";
 import { requireAuth } from "../middlewares/auth.middleware";
+import { validate } from '../middlewares/validate.middleware';
+import { createTransactionSchema } from '../schemas/transaction.schema';
 
 const router = Router();
 
-router.post("/", requireAuth, createTransactionEndpoint);
+router.post("/", validate(createTransactionSchema), requireAuth, createTransactionEndpoint);
 router.get("/", requireAuth, getHistory);
 router.get("/confirm/:token", confirmTransactionEndpoint);
 
